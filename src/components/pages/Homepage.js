37b8383f5300f2d8db6e5620f399afa8aa8ef2d0
Daniel Ramirez <dramirtek99@gmail.com>
2023-02-05 20:11:02 -0800

    setup skeleton for todo list react app

---
+++ src/components/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './Homepage.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a sample home page for a React application.</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/pages/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Todo.css';
+
+const Todo = () => {
+    return(
+        <>
+            <h1>Welcome to the To Do Page</h1>
+            <p>This is a sample To Do page for a React application.</p>
+        </>
+    );
+};
+
+export default Todo;
