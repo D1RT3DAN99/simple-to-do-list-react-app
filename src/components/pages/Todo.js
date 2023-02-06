5b9db0bff9844ca9d9e1b484f15516a365cb28fa
Daniel Ramirez <dramirtek99@gmail.com>
2023-02-06 13:23:34 -0800

    added todo list functionality

--- src/components/pages/Todo.js
+++ src/components/pages/Todo.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Todo.css';
 
 const Todo = () => {
-    return(
-        <>
-            <h1>Welcome to the To Do Page</h1>
-            <p>This is a sample To Do page for a React application.</p>
-        </>
-    );
+    const [todos, setTodos] = useState([
+        { id: 1, text: 'Learn React' },
+        { id: 2, text: 'Build a To-Do List app' },
+        { id: 3, text: 'Enjoy coding' }
+    ]);
+
+    const addTodo = (text) => {
+        const newTodos = [...todos, { id: todos.length + 1, text }];
+        setTodos(newTodos);
+    };
+
+    const removeTodo = (id) => {
+        const updatedTodos = todos.filter(todo => todo.id !== id);
+        setTodos(updatedTodos);
+      };
+
+    return (
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            {todo.text}{' '}
+            <button onClick={() => removeTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={e => {
+        e.preventDefault();
+        addTodo(e.target.todo.value);
+        e.target.todo.value = '';
+      }}>
+        <input type="text" name="todo" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
 };
 
 export default Todo;
